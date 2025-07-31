65276106bc7432ac0fa5d8a86434cc72cd1c9a98
rohan sujeesh <rohansujeesh@Mac.fritz.box>
2025-07-31 23:47:48 +0200

    updated model library and exploration notebook

--- notebooks/model_Library_Rohan.py
+++ notebooks/model_Library_Rohan.py
@@ -15,8 +15,19 @@ Running collection of DL model definitions
 in use in the python notebooks
 
 Author: Rohan
-Stand: 22/07/2025
+Stand: 31/07/2025
 """
+################################################
+#loss model functions
+def dice_coef(y_true,y_pred, smooth=1):
+    y_true_f = k.flatten(y_true)
+    y_pred_f = k.flatten(y_pred)
+    intersection = k.sum(y_true_f*y_pred_f)
+
+    return (2*intersection + smooth) / (k.sum(y_true_f)+k.sum(y_pred_f) + smooth)
+
+
+################################################
 
 def base_unet_test(input_shape=(512,512,1)):
     inputs = tf.keras.Input(input_shape)
@@ -70,5 +81,61 @@ def base_unet_test(input_shape=(512,512,1)):
 
     model.compile(optimizer='adam',
               loss='binary_crossentropy',
-              metrics=['accuracy'])
+              metrics=['accuracy',dice_coef])
+    return model
+
+
+def small_unet_test(input_shape=(256,256,1)):
+    inputs = tf.keras.Input(input_shape)
+
+    # Encoder
+    c1 = layers.Conv2D(32, 3, activation='relu', padding='same')(inputs)
+    c1 = layers.Conv2D(32, 3, activation='relu', padding='same')(c1)
+    p1 = layers.MaxPooling2D((2, 2))(c1)
+
+    c2 = layers.Conv2D(64, 3, activation='relu', padding='same')(p1)
+    c2 = layers.Conv2D(64, 3, activation='relu', padding='same')(c2)
+    p2 = layers.MaxPooling2D((2, 2))(c2)
+    
+    c3 = layers.Conv2D(128, 3, activation='relu', padding='same')(p2)
+    c3 = layers.Conv2D(128, 3, activation='relu', padding='same')(c3)
+    p3 = layers.MaxPooling2D((2, 2))(c3)
+
+    c4 = layers.Conv2D(256, 3, activation='relu', padding='same')(p3)
+    c4 = layers.Conv2D(256, 3, activation='relu', padding='same')(c4)
+    p4 = layers.MaxPooling2D(pool_size=(2, 2))(c4)
+    
+    # Bottleneck
+    c5 = layers.Conv2D(512, 3, activation='relu', padding='same')(p4)
+    c5 = layers.Conv2D(512, 3, activation='relu', padding='same')(c5)
+
+    # Decoder
+    
+    u6 = layers.Conv2DTranspose(256, 2, strides=(2, 2), padding='same')(c5)
+    u6 = layers.concatenate([u6, c4])
+    c6 = layers.Conv2D(256, 3, activation='relu', padding='same')(u6)
+    c6 = layers.Conv2D(256, 3, activation='relu', padding='same')(c6)
+
+    u7 = layers.Conv2DTranspose(128, 2, strides=(2, 2), padding='same')(c6)
+    u7 = layers.concatenate([u7, c3])
+    c7 = layers.Conv2D(128, 3, activation='relu', padding='same')(u7)
+    c7 = layers.Conv2D(128, 3, activation='relu', padding='same')(c7)
+    
+    u8 = layers.Conv2DTranspose(64, 2, strides=(2, 2), padding='same')(c7)
+    u8 = layers.concatenate([u8, c2])
+    c8 = layers.Conv2D(64, 3, activation='relu', padding='same')(u8)
+    c8 = layers.Conv2D(64, 3, activation='relu', padding='same')(c8)
+
+    u9 = layers.Conv2DTranspose(32, 2, strides=(2, 2), padding='same')(c8)
+    u9 = layers.concatenate([u9, c1])
+    c9 = layers.Conv2D(32, 3, activation='relu', padding='same')(u9)
+    c9 = layers.Conv2D(32, 3, activation='relu', padding='same')(c9)
+
+    outputs = layers.Conv2D(1, 1, activation='sigmoid')(c9)
+
+    model = models.Model(inputs=[inputs], outputs=[outputs])
+
+    model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy', dice_coef])
     return model
